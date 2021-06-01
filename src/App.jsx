f1fa58d9bc9deeffd4ee8d5601495b90f5e55c55
Po Chen <pseudopochen@gmail.com>
2021-05-31 22:48:49 -0600

    configuer craco to use antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import { Button, message } from "antd";
+// import 'antd/dist/antd.less' // no need to import here because of babel-plugin-import in craco.config.js
 
 export default class App extends Component {
+  handleClick = () => {
+    message.success("Success!");
+  };
+
   render() {
-    return <div>App...</div>;
+    return (
+      <Button type="primary" onClick={this.handleClick}>
+        Primary
+      </Button>
+    );
   }
 }
